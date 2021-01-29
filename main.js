f8789282a7216862b8c712f63009aed968d81d96
cobachie <7006340+cobachie@users.noreply.github.com>
2021-01-29 10:25:35 +0900

    add a process to add a new task

--- main.js
+++ main.js
@@ -20,6 +20,18 @@ const app = new Vue({
     todos: []
   },
   methods: {
+    add(event, value) {
+      const comment = this.$refs.comment
 
+      if (!comment.value.length) return
+
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0
+      })
+
+      comment.value = ""
+    }
   }
 })

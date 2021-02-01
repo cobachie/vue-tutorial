5539fae866bd9cf45e9b7f91c8de5db8b218695c
cobachie <7006340+cobachie@users.noreply.github.com>
2021-02-01 14:25:01 +0900

    add a radio button to filter todos

--- main.js
+++ main.js
@@ -17,7 +17,13 @@ const todoStorage = {
 const app = new Vue({
   el: "#app",
   data: {
-    todos: []
+    todos: [],
+    options: [
+      { value: -1, label: 'すべて' },
+      { value:  0, label: '作業中' },
+      { value:  1, label: '完了' },
+    ],
+    current: -1
   },
   created() {
     this.todos = todoStorage.fetch()
